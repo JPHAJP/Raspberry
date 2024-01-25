b9b1b9f3a011da882a9aed44d6a847149d1796b3
jpha <josepabloha@live.com.mx>
2024-01-25 11:45:00 -0600

    update

--- memorias.py
+++ memorias.py
@@ -1,22 +1,12 @@
 #Manejo de procesos de memoria
 # Autor: José Pablo Hernández Alonso
-
 from time import sleep
-
-ListaTareas = []
-DiccionarioMemoriaProcesos = {}
-
-def AgregarMemoria(tarea):              
-    for i in range(len(tarea)):                                 #Agrega a un diccionario para separar tareas de complejidad y crear subprocesos
-        DiccionarioMemoriaProcesos[i+1]=int(tarea[i])
-    #print(DiccionarioMemoriaProcesos)
-    PROCESAR(DiccionarioMemoriaProcesos)                        #Manda a llamar a la función RISC para ejecutar los subprocesos
+DiccionarioMemoriaProcesos = {}                      
 
 def PROCESAR(DiccionarioMemoriaProcesos):                       #Función para ejecutar los subprocesos
     print(f'\nSe tienen {len(DiccionarioMemoriaProcesos)} tarea(s) por ejecutar')
     for i in range(len(DiccionarioMemoriaProcesos)):
         print(f'\nEjecutando tarea_{i+1}')
-        
         if DiccionarioMemoriaProcesos[i+1] > 20:
             print(f'La tarea_{i+1} es muy compleja no se puede ejecutar, se eliminará de la memoria')
             sleep(1)
@@ -44,8 +34,7 @@ def CISC(DiccionarioMemoriaProcesos,tarea):
 Tareas_por_realizar=input('Introduzca el número de tareas por realizar: ')
 for i in range(int(Tareas_por_realizar)):
     tarea=int(input(f'Introduzca la complejidad de la tarea_{i+1}: '))
-    ListaTareas.append(tarea)
-#print(ListaTareas)
-AgregarMemoria(ListaTareas)
+    DiccionarioMemoriaProcesos[i+1]=int(tarea)
+PROCESAR(DiccionarioMemoriaProcesos) 
 #print(DiccionarioMemoriaProcesos)
-print('Se han ejecutado todos los subprocesos y las tareas han sido eliminadas de la memoria')
+print('\nSe han ejecutado todos los subprocesos y las tareas han sido eliminadas de la memoria')
