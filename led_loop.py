4da254ad7785d586aa5b68be4e5f27b176c76cf3
jpha <josepabloha@live.com.mx>
2024-02-06 12:23:48 -0600

    update loop 2

--- led_loop.py
+++ led_loop.py
@@ -1,37 +1,24 @@
-# from gpiozero import LED
-# from time import sleep
-
-# # Create an LED object for GPIO pin 11
-# led = LED(17)  # Use the GPIO pin number, not the physical pin number
-# try:
-#     while True:
-#         # Turn on the LED
-#         led.on()
-#         print("LED on")
-#         # Wait for a few seconds
-#         sleep(1)
-#         # Turn off the LED
-#         led.off()
-#         print("LED off")
-#         # Wait for a few seconds
-#         sleep(1)
-# except KeyboardInterrupt:
-#     # If the user presses Ctrl+C, gracefully exit the loop
-#     print("\nExiting the program.")
-# finally:
-#     # Turn off the LED and clean up
-#     led.off()
-#     print("LED off")
-
-#------------------------------------#
-
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
-
-while True:
-    led.on()
-    sleep(1)
+# Create an LED object for GPIO pin 11
+led = LED(17)  # Use the GPIO pin number, not the physical pin number
+try:
+    while True:
+        # Turn on the LED
+        led.on()
+        print("LED on")
+        # Wait for a few seconds
+        sleep(1)
+        # Turn off the LED
+        led.off()
+        print("LED off")
+        # Wait for a few seconds
+        sleep(1)
+except KeyboardInterrupt:
+    # If the user presses Ctrl+C, gracefully exit the loop
+    print("\nExiting the program.")
+finally:
+    # Turn off the LED and clean up
     led.off()
-    sleep(1)
+    print("LED off")
