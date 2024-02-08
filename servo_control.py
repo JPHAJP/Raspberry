dfb482a644c54b4bbb02d9fecc485f44bb9edc28
jpha <josepabloha@live.com.mx>
2024-02-08 12:34:30 -0600

    angle modify

--- servo_control.py
+++ servo_control.py
@@ -4,13 +4,13 @@ from time import sleep
 servo = AngularServo(17, min_angle=-90, max_angle=90)
 
 while True:
-    servo.angle = -90
-    sleep(2)
-    servo.angle = -45
-    sleep(2)
     servo.angle = 0
     sleep(2)
     servo.angle = 45
     sleep(2)
     servo.angle = 90
+    sleep(2)
+    servo.angle = 135
+    sleep(2)
+    servo.angle = 180
     sleep(2)
