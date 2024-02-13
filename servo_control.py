b2e9166a50fb3bfc1c0f2941e91177c3b5fde9cc
jpha <josepabloha@live.com.mx>
2024-02-13 11:55:04 -0600

    p1

--- servo_control.py
+++ servo_control.py
@@ -1,12 +1,26 @@
-from gpiozero import AngularServo
+# from gpiozero import AngularServo
+# from time import sleep
+
+# servo = AngularServo(17, min_angle=-90, max_angle=90)
+
+# while True:
+#     servo.angle = -90
+#     sleep(2)
+#     servo.angle = 0
+#     sleep(2)
+#     servo.angle = 90
+#     sleep(2)
+
+
+from gpiozero import Servo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = Servo(17)
 
 while True:
-    servo.angle = -90
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = 90
-    sleep(2)
+    servo.min()
+    sleep(1)
+    servo.mid()
+    sleep(1)
+    servo.max()
+    sleep(1)
