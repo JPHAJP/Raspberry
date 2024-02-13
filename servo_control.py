41d48e3d9e9c7c4c6ac7d1a546f6915ef32eeee8
jpha <josepabloha@live.com.mx>
2024-02-13 12:24:05 -0600

    p6

--- servo_control.py
+++ servo_control.py
@@ -44,17 +44,25 @@ maxPW=(2.0+myCorrection)/1000
 minPW=(1.0-myCorrection)/1000
  
 servo = Servo(myGPIO,min_pulse_width=minPW,max_pulse_width=maxPW)
- 
+
 while True:
-    servo.mid()
-    print("mid")
-    sleep(0.5)
-    servo.min()
-    print("min")
-    sleep(1)
-    servo.mid()
-    print("mid")
-    sleep(0.5)
-    servo.max()
-    print("max")
-    sleep(1)
+    value = input("Enter a value between -1 and 1: ")
+    value = float(value)
+    if value < -1 or value > 1:
+        print("Invalid input")
+    else:
+        servo.value = value
+        sleep(2)
+# while True:
+#     servo.mid()
+#     print("mid")
+#     sleep(1)
+#     servo.min()
+#     print("min")
+#     sleep(1)
+#     servo.mid()
+#     print("mid")
+#     sleep(1)
+#     servo.max()
+#     print("max")
+#     sleep(1)
