da89e00eb9f916ae72cc620f569435e577dacea3
jpha <josepabloha@live.com.mx>
2024-02-13 12:26:54 -0600

    p7

--- servo_control.py
+++ servo_control.py
@@ -1,45 +1,9 @@
-# from gpiozero import AngularServo
-# from time import sleep
-# servo = AngularServo(17, min_angle=-90, max_angle=90)
-# while True:
-#     servo.angle = -90
-#     sleep(2)
-#     servo.angle = 0
-#     sleep(2)
-#     servo.angle = 90
-#     sleep(2)
-
-
-# while True:
-#     servo.min()
-#     sleep(1)
-#     servo.mid()
-#     sleep(1)
-#     servo.max()
-#     sleep(1)
-
-# from gpiozero import Servo
-# from time import sleep
-
-# servo = Servo(17)
-# while True:
-#     value = input("Enter a value between -1 and 1: ")
-#     value = float(value)
-#     servo.value = value
-#     sleep(2)
-
-#     # if value < -1 or value > 1:
-#     #     print("Invalid input")
-#     # else:
-#     #     servo.value = value
-#     #     sleep(2)
-
 from gpiozero import Servo
 from time import sleep
  
 myGPIO=17
  
-myCorrection=0.40
+myCorrection=0.50
 maxPW=(2.0+myCorrection)/1000
 minPW=(1.0-myCorrection)/1000
  
