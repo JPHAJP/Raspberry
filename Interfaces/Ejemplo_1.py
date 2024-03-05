e744e0cabe61d0658a232769b46cc20bd29bb8a5
jpha <josepabloha@live.com.mx>
2024-03-05 12:58:05 -0600

    up

--- Interfaces/Ejemplo_1.py
+++ Interfaces/Ejemplo_1.py
@@ -16,3 +16,14 @@ def on_button_clicked():
 button.clicked.connect(on_button_clicked)
 button.show()
 app.exec()
+
+# from PyQt6.QtWidgets import *
+
+# count_clicks = [0]  # Usamos una lista para almacenar el conteo de clics
+
+# app = QApplication([])
+# button = QPushButton('Click')
+
+# button.clicked.connect(lambda: (QMessageBox().setText(f'clicked {count_clicks[0]} times!').exec(), count_clicks.__setitem__(0, count_clicks[0] + 1)))
+# button.show()
+# app.exec()
